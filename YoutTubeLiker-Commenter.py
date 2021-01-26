e12144dfe7f1a5f5286206cae0a798e5ae773810
vikramsodhan <vikramjot12@gmail.com>
2021-01-26 15:53:09 -0800

    Got rid of random comments ready for public GitHub

--- YoutTubeLiker-Commenter.py
+++ YoutTubeLiker-Commenter.py
@@ -37,7 +37,7 @@ if not credentials or not credentials.valid:
 
 youtube = build("youtube", "v3", credentials=credentials)
 
-
+# gets a list of all the videos of a channel usings it's channel id
 def get_channel_videos(channel_id):
     resource = youtube.channels().list(id=channel_id, part="contentDetails").execute()
 
@@ -68,24 +68,9 @@ def get_channel_videos(channel_id):
 
 
 # Update the string with desired youtube channel id
-# BballBreakdown UCSpvjDk06HLxBaw8sZw7SkA
-# Booksin5 UCxambigWU3QMK9UJXU2mwPg
-videos_list = get_channel_videos("UCSpvjDk06HLxBaw8sZw7SkA")
+videos_list = get_channel_videos("Desired-YouTube-Id")
 
-comment_list = [
-    "I love the content keep it up!!",
-    "Very informational thank you!",
-    "Amazing work!",
-    "I love this!!",
-    "Thank you clear explanation",
-    "Awesome content keep it up!!",
-    "Absolutely love the 5 minute length!",
-    "This is very insightful, thanks for sharing",
-    "Great job keep it up.",
-    "I really enjoy these videos.",
-]
-
-
+# comments on the particular video
 def comment_on_videos(videoId, channelId, comment):
     request = youtube.commentThreads().insert(
         part="snippet",
@@ -103,48 +88,21 @@ def comment_on_videos(videoId, channelId, comment):
     )
 
     response = request.execute()
-    # print(response)
 
 
-def update_comment_videos(comment, text):
-    comment["snippet"]["topLevelComment"]["snippet"]["textOriginal"] = text
-    update_result = youtube.commentThreads().update(part="snippet", body=comment)
-    # request = youtube.commentThreads().update(
-    #         part="snippet",
-    #         body={
-    #             "id":commentId,
-    #             "snippet" :{
-    #                 "videoId": videoId,
-    #                 "channelId": channelId,
-    #                 "topLevelComment": {
-    #                     "snippet": {
-    #                         "textOriginal": comment,
-    #                     }
-    #                 }
-    #             }
-    #         }
-    #     )
-
-    response = update_result.execute()
-    # print(response)
-
-
-def get_comments(video_Id, channel_Id):
-    request = youtube.commentThreads().list(
-        part="snippet",
-        videoId=video_Id,
-    )
-    response = request.execute()
-    return response
-    # print(response)
-
-
-# comment_we_want_to_update=get_comments(videos_list[1]["snippet"]["resourceId"]["videoId"], videos_list[0]["snippet"]["channelId"])["items"][0]
-video_id_comment_check = videos_list[0]["snippet"]["resourceId"]["videoId"]
-channel_id_comment_check = videos_list[0]["snippet"]["channelId"]
-# for index in range(47):
-#     comment_on_videos(video_id_comment_check, channel_id_comment_check, str(index))
-#     print(index)
+# List of generic comments to comment on videos
+comment_list = [
+    "I love the content keep it up!!",
+    "Very informational thank you!",
+    "Amazing work!",
+    "I love this!!",
+    "Thank you clear explanation",
+    "Awesome content keep it up!!",
+    "Absolutely love the 5 minute length!",
+    "This is very insightful, thanks for sharing",
+    "Great job keep it up.",
+    "I really enjoy these videos.",
+]
 
 for index, video in enumerate(videos_list):
     # this is to like the videos
@@ -161,47 +119,20 @@ for index, video in enumerate(videos_list):
             comment=comment_list[index % len(comment_list)],
         )
 
-
-# print(comment_we_want_to_update["snippet"]["topLevelComment"]["snippet"]["textOriginal"])
-
-
-# comment_on_videos(
-#     videoId=videos_list[0]["snippet"]["resourceId"]["videoId"],
-#     channelId=videos_list[0]["snippet"]["channelId"],
-#     comment="testing response code"
-# )
-
-# for index, video in enumerate(videos_list):
-#     #youtube.videos().rate(id=video["snippet"]["resourceId"]["videoId"], rating="like").execute()
-#     #print(videos_list[index % len(videos_list)])
-#     # print(video["snippet"]["resourceId"]["videoId"])
-#     # print(video["snippet"]["channelId"])
-#     # print(comment_list[index % len(comment_list)])
-#     print(index)
-#     comment_on_videos(
-#         videoId=video["snippet"]["resourceId"]["videoId"],
-#         channelId=video["snippet"]["channelId"],
-#         comment=comment_list[index % len(comment_list)])
-
-# for index in range(0, 47):
-#     print(index)
-#     update_comment_videos(
-#         commentId="UgyyPq19Gf9T6-xlYFJ4AaABAg",
-#         videoId=videos_list[0]["snippet"]["resourceId"]["videoId"],
-#         channelId=videos_list[0]["snippet"]["channelId"],
-#         comment="")
-
-# youtube.videos().rate(id=videos_list[0]["snippet"]["resourceId"]["videoId"], rating="like").execute()
+# not used in this program but
+# Takes a comment and updates it's to become text
+def update_comment_videos(comment, text):
+    comment["snippet"]["topLevelComment"]["snippet"]["textOriginal"] = text
+    update_result = youtube.commentThreads().update(part="snippet", body=comment)
+    response = update_result.execute()
 
 
-# videos_list[0]["snippet"]["resourceId"]["videoId"]
-# videos_list[0]["snippet"]["channelId"]
-
-# "nJ384wg3g5A" videos_list[0]["id"]
-# request = youtube.playlistItems().list(
-#     part="snippet", playlistId="UUxambigWU3QMK9UJXU2mwPg"
-# )
-
-# response = request.execute()
-
-# print(response)
+# not used in this program but
+# gets a list of comments on a particular YouTube video
+def get_comments(video_Id, channel_Id):
+    request = youtube.commentThreads().list(
+        part="snippet",
+        videoId=video_Id,
+    )
+    response = request.execute()
+    return response
